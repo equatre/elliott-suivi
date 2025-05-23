ac618bd73b8048cd005685065af33c5ec9fa09f2
equatre <elliott6662@hotmail.com>
2025-05-23 16:44:39 +0200

    Corrige l'écoute Flask pour Render (0.0.0.0 + PORT)

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json
@@ -21,4 +22,5 @@ def get_position():
         return jsonify(json.load(f))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # par défaut : 5000
+    app.run(debug=True, host='0.0.0.0', port=port)
